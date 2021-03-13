a156c2662dbf168591ef0caaefc1ffb1eb949dfd
tacticaltofu <dingkevin@gmail.com>
2021-03-13 12:57:30 -0500

    Check NODE_ENV to see if site is in prod

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const config = {
-  baseURL: process.env.REACT_APP_BACKEND_URL
-};
+const baseURL = process.env.NODE_ENV === 'production' ?
+  process.env.REACT_APP_BACKEND_URL : 'http://localhost:5000';
+
+const config = { baseURL };
 
 const instance = axios.create(config);
 
