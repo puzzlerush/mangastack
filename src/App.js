ee4913346f4ed0edc949f3a0a2544e5cdcd839b3
tacticaltofu <dingkevin@gmail.com>
2021-06-04 13:42:59 -0400

    create basic site maintenance page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
+import { Paper, Typography, Box, Link } from '@material-ui/core';
 import AppRouter from './routers/AppRouter';
 import getTheme from './themes/themes';
 
@@ -9,7 +9,32 @@ function App({ theme }) {
     <div className="App">
       <ThemeProvider theme={getTheme(theme)}>
         <Paper elevation={0} style={{ minHeight: '100vh' }}>
-          <AppRouter />
+          {/* <AppRouter /> */}
+          <Box paddingY={7} paddingX={2}>
+            <Typography paragraph variant="h3" align="center">
+              We'll be back!
+            </Typography>
+            <Typography paragraph variant="subtitle1" align="center">
+              Unfortunately, it has been brought to my attention that this site
+              has been causing DMCA issues for{' '}
+              <Link href="https://api.mangadex.org/docs.html#section/Reading-a-chapter-using-the-API/Retrieving-pages-from-the-MangaDex@Home-network">
+                MangaDex@Home
+              </Link>{' '}
+              clients.
+            </Typography>
+            <Typography paragraph variant="subtitle1" align="center">
+              Therefore, I have decided to temporarily shut down{' '}
+              <Link href="http://mangastack.cf/">mangastack.cf</Link>, until I
+              can figure out a workaround.
+            </Typography>
+            <Typography paragraph variant="subtitle1" align="center">
+              If you have any questions, feel free to contact me on{' '}
+              <Link href="https://www.reddit.com/user/dalkerkd/">reddit</Link>.
+            </Typography>
+            <Typography paragraph variant="h4" align="center">
+              Thanks for your understanding!
+            </Typography>
+          </Box>
         </Paper>
       </ThemeProvider>
     </div>
@@ -17,7 +42,7 @@ function App({ theme }) {
 }
 
 const mapStateToProps = (state) => ({
-  theme: state.settings.theme
+  theme: state.settings.theme,
 });
 
 export default connect(mapStateToProps)(App);
